94bf6258b073ed666a14839a9ec1fe792f966923
basharat <uzair5399@gmail.com>
2026-05-24 22:13:10 -0500

    Add requist validation for transfer creation

--- src/main/java/org/example/securepayai/controller/TransferController.java
+++ src/main/java/org/example/securepayai/controller/TransferController.java
@@ -1,10 +1,12 @@
 package org.example.securepayai.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.securepayai.dto.CreateTransferRequest;
 import org.example.securepayai.dto.CreatedTransferResponse;
 import org.example.securepayai.service.TransferService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,7 @@ public class TransferController {
     private final TransferService service;
 
     @PostMapping
-    public CreatedTransferResponse createTransfer(CreateTransferRequest request) {
+    public CreatedTransferResponse createTransfer(@Valid @RequestBody CreateTransferRequest request) {
         return service.creatTransfer(request);
     }
 }
